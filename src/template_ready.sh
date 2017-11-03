d7b8250a8385be643132b0c97a7c502c9c737d5c
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-03 23:28:42 +0100

    if was not working

--- src/template_ready.sh
+++ src/template_ready.sh
@@ -12,7 +12,7 @@ for key in "${!CONTAINER_DOMAINS[@]}"; do
   DOMAIN=$CONTAINER_DOMAINS[$key]
 
   # If the container contains a VIRTUAL_HOST entry, then generate the SSL
-  if [[ $CONTAINER_ISVHOST ]]; then
+  if [[ $CONTAINER_ISVHOST == 1 ]]; then
     # Domain may be declared with a port, if so, get only the domain value
     if [[ $DOMAIN == *":"* ]]; then
       IFS=, read -ra TMP <<< "$DOMAIN"

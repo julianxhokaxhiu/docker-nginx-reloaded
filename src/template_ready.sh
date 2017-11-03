36722d8d129f21343eeffc8e59ccb3c1d3c754da
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-03 23:00:59 +0100

    Generate SSL only if domain is a valid FQDN

--- src/template_ready.sh
+++ src/template_ready.sh
@@ -10,24 +10,27 @@ IFS=, read -ra CONTAINER_DOMAINS <<< "$CONTAINER_DOMAIN"
 for key in "${!CONTAINER_DOMAINS[@]}"; do
   DOMAIN=$CONTAINER_DOMAINS[$key]
 
-  # Domain may be declared with a port, if so, get only the domain value
-  if [[ $DOMAIN == *":"* ]]; then
-    IFS=, read -ra TMP <<< "$DOMAIN"
-    DOMAIN=$TMP[0]
+  # If it is a valid FQDN then process for SSL generation
+  if [[ ! -z `echo $DOMAIN | grep -P '(?=^.{1,254}$)(^(?>(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)'` ]]; then
+    # Domain may be declared with a port, if so, get only the domain value
+    if [[ $DOMAIN == *":"* ]]; then
+      IFS=, read -ra TMP <<< "$DOMAIN"
+      DOMAIN=$TMP[0]
+    fi
+
+    # Generate the SSL certificate
+    /root/.acme.sh/acme.sh \
+      --issue \
+      -d $DOMAIN \
+      -w /var/www/localhost/htdocs
+
+    # Install SSL certificate
+    /root/.acme.sh/acme.sh \
+      --install-cert \
+      -d $DOMAIN \
+      --key-file /etc/nginx/certs/$DOMAIN.key \
+      --fullchain-file /etc/nginx/certs/$DOMAIN.crt
   fi
-
-  # Generate the SSL certificate
-  /root/.acme.sh/acme.sh \
-    --issue \
-    -d $DOMAIN \
-    -w /var/www/localhost/htdocs
-
-  # Install SSL certificate
-  /root/.acme.sh/acme.sh \
-    --install-cert \
-    -d $DOMAIN \
-    --key-file /etc/nginx/certs/$DOMAIN.key \
-    --fullchain-file /etc/nginx/certs/$DOMAIN.crt
 done
 
 # Reload nginx service
